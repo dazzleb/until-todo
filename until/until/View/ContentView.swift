50ffd4249510130823a8b5cc337c2c5cfad034a6
dazzle <dazzledazzleb@gmail.com>
2022-12-15 17:36:47 +0900

    feat: ContentView 에 각 뷰 추가

--- until/until/View/ContentView.swift
+++ until/until/View/ContentView.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var items: [String] = [
+        "와칸다 포에버!",
+        "아바타 꿀잼",
+        "물의 길...☃️"
+    ]
+
     var body: some View {
         VStack {
+            /// 오늘 날짜를 표현 하는 View
             TodayView()
             Spacer()
-            
+            // 오늘 할일을 적을 View
+            TodoTextFieldView()
+            Spacer()
+
+                /// List view
+                List{
+                    ForEach(items, id: \.self) { item in
+                            ListView(title: item)
+                    }
+                }.listStyle(PlainListStyle())
+                        
         }
         .padding()
     }
@@ -26,3 +43,9 @@ struct ContentView_Previews: PreviewProvider {
         }
     }
 }
+
+
+
+
+
+
