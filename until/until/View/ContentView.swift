58dbe8a32d81bf1d1bc76d21118b49aa08a9bea6
dazzle <dazzledazzleb@gmail.com>
2022-12-15 16:32:58 +0900

    feat: ContentView Preview에 Nvigation 추가 본문: NavigationView 추가로 프리뷰 에서도 확인가능

--- until/until/View/ContentView.swift
+++ until/until/View/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("오늘까지!")
+            Text("HI")
         }
         .padding()
     }
@@ -21,6 +18,8 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            ContentView()
+        }
     }
 }
