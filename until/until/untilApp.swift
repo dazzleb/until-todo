7f93cecc0f79b8e7e6976a98365e52d47375adff
dazzle <dazzledazzle@gmail.com>
2022-12-15 16:09:22 +0900

    feat: 프로젝트 생성

---
+++ until/until/untilApp.swift
@@ -0,0 +1,17 @@
+//
+//  untilApp.swift
+//  until
+//
+//  Created by 시혁 on 2022/12/15.
+//
+
+import SwiftUI
+
+@main
+struct untilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
